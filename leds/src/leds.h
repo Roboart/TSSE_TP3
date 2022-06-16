2753bf14cc5259406a8247ce97392295b39fe6f2
Roboart <dr.osvaldo@gmail.com>
2022-06-16 12:56:40 -0400

    Se agrega comentarios Doxygen en prototipos de funcion.

--- leds/src/leds.h
+++ leds/src/leds.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+
 /**
  * @brief Configura la biblioteca y apaga todos los leds
  * 
@@ -11,13 +12,38 @@
 void LedsInit(uint16_t *direccion);
 
 /**
- * @brief 
+ * @brief Enciende led solicitado
  * 
- * @param led 
+ * @param led Numero de led que se requiere encender
  */
 void LedTurnOn(uint8_t led);
+
+/**
+ * @brief apaga led solicitado
+ * 
+ * @param led Numero de led que se requiere apagar
+ */
 void LedTurnOff(uint8_t led);
+
+/**
+ * @brief Enciende todos los leds
+ * 
+ * @param direccion Direccion del puerto GPIO que controla todos los leds.
+ */
 void LedTurnOnAll(uint16_t *direccion);
+
+/**
+ * @brief Enciende led solicitado
+ * 
+ * @param led Numero de led que se requiere encender
+ */
 void LedTurnOffAll(uint16_t *direccion);
+
+/**
+ * @brief Apaga todos los leds
+ * 
+ * @param direccion Direccion del puerto GPIO que controla todos los leds.
+ */
 bool LedGetState(uint8_t led);
+
 #endif
